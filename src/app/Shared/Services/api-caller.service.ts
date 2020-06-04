a9983709f084fba972e4d98f6b99643ef1aee958
wspot <iwspot1@gmail.com>
2020-06-04 17:25:52 +0100

    app structure : http handler service

---
+++ src/app/Shared/Services/api-caller.service.ts
@@ -0,0 +1,23 @@
+import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { switchMap,catchError } from 'rxjs/operators'; 
+import { of } from 'rxjs';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class ApiCallerService {
+
+  public serverAddress : string = "";
+  public serviceName : string = "assets/data.json";
+
+  constructor(private httpService: HttpClient) { }
+
+  
+  public callGetService(servicename : string){
+    return this.httpService.get(this.serverAddress + this.serviceName).pipe(
+      catchError( () => { return of(null) })
+    );
+  }
+
+}
